54f6ed958bd9a50fc10afd363bc33eb5c9eb7bb6
Bhavesh Garg <54619203+bhavesh1129@users.noreply.github.com>
2020-10-01 02:10:01 +0530

    Adding More Emojis

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -1,93 +1,154 @@
-const emojipedia = [
-  {
-    id: 1,
-    emoji: "💪",
-    name: "Tense Biceps",
-    meaning:
-      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
-  },
-  {
-    id: 2,
-    emoji: "🙏",
-    name: "Person With Folded Hands",
-    meaning:
-      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
-  },
-  {
-    id: 3,
-    emoji: "🤣",
-    name: "Rolling On The Floor, Laughing",
-    meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-  },
-  {
-    id: 4,
-    emoji: "😎",
-    name: "Face With Sunglasses",
-    meaning:
-      "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach.“."
-  },
-  {
-    id: 5,
-    emoji: "😉",
-    name: "Winking Face",
-    meaning:
-      "A yellow face with a slight smile shown winking, usually its left eye. May signal a joke, flirtation, hidden meaning, or general positivity. Tone varies, including playful, affectionate, suggestive, or ironic.“."
-  },
-  {
-    id: 6,
-    emoji: "😐 ",
-    name: "Neutral Face",
-    meaning:
-      "A yellow face with simple, open eyes and a flat, closed mouth. Intended to depict a neutral sentiment but often used to convey mild irritation and concern or a deadpan sense of humor.“."
-  },
-  {
-    id: 7,
-    emoji: "😷 ",
-    name: "Face with Medical Mask",
-    meaning:
-      "A yellow face with closed eyes wearing a white surgical mask, as used by health workers in hospitals. Also worn to avoid sickness or spreading airborne diseases.“."
-  },
-  {
-    id: 8,
-    emoji: "💔",
-    name: "Broken Heart",
-    meaning:
-      "A love heart, broken in two. This emoji represents the aching one feels when they are missing the  person they love.“."
-  },
-  {
-    id: 9,
-    emoji: "♊",
-    name: "Gemini",
-    meaning:
-      "The Gemini astrological sign in the Zodiac. This Zodiac Symbol represents Twins.“."
-  },
-  {
-    id: 10,
-    emoji: "🙈",
-    name: " See-No-Evil Monkey",
-    meaning:
-      "The see no evil monkey, called Mizaru (Japanese for “see not”), one of the Three Wise Monkeys. Depicted as the brown 🐵 Monkey Face with tan or pinkish hands covering its eyes.“."
-  },
-  {
-    id: 11,
-    emoji: "🤮",
-    name: "Face Vomiting",
-    meaning: "A yellow face with scrunched eyes spewing birght-green vomit. This emoji represents illness or disgust.“."
-  },
-  {
-    id: 12,
-    emoji: "🥰",
-    name: "Smiling Face with Hearts",
-    meaning: "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love."
+const emojipedia = [{
+        id: 1,
+        emoji: "💪",
+        name: "Tense Biceps",
+        meaning: "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+        id: 2,
+        emoji: "🙏",
+        name: "Person With Folded Hands",
+        meaning: "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+        id: 3,
+        emoji: "🤣",
+        name: "Rolling On The Floor, Laughing",
+        meaning: "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+        id: 4,
+        emoji: "😎",
+        name: "Face With Sunglasses",
+        meaning: "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach.“."
+    },
+    {
+        id: 5,
+        emoji: "😉",
+        name: "Winking Face",
+        meaning: "A yellow face with a slight smile shown winking, usually its left eye. May signal a joke, flirtation, hidden meaning, or general positivity. Tone varies, including playful, affectionate, suggestive, or ironic.“."
+    },
+    {
+        id: 6,
+        emoji: "😐 ",
+        name: "Neutral Face",
+        meaning: "A yellow face with simple, open eyes and a flat, closed mouth. Intended to depict a neutral sentiment but often used to convey mild irritation and concern or a deadpan sense of humor.“."
+    },
+    {
+        id: 7,
+        emoji: "😷 ",
+        name: "Face with Medical Mask",
+        meaning: "A yellow face with closed eyes wearing a white surgical mask, as used by health workers in hospitals. Also worn to avoid sickness or spreading airborne diseases.“."
+    },
+    {
+        id: 8,
+        emoji: "💔",
+        name: "Broken Heart",
+        meaning: "A love heart, broken in two. This emoji represents the aching one feels when they are missing the  person they love.“."
+    },
+    {
+        id: 9,
+        emoji: "♊",
+        name: "Gemini",
+        meaning: "The Gemini astrological sign in the Zodiac. This Zodiac Symbol represents Twins.“."
+    },
+    {
+        id: 10,
+        emoji: "🙈",
+        name: " See-No-Evil Monkey",
+        meaning: "The see no evil monkey, called Mizaru (Japanese for “see not”), one of the Three Wise Monkeys. Depicted as the brown 🐵 Monkey Face with tan or pinkish hands covering its eyes.“."
+    },
+    {
+        id: 11,
+        emoji: "🤮",
+        name: "Face Vomiting",
+        meaning: "A yellow face with scrunched eyes spewing birght-green vomit. This emoji represents illness or disgust.“."
+    },
+    {
+        id: 12,
+        emoji: "🥰",
+        name: "Smiling Face with Hearts",
+        meaning: "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love."
 
-  },
-  {
-    id: 13,
-    emoji: "🥺",
-    name: "Pleading Face",
-    meaning: "A yellow face with furrowed eyebrows, a small frown, and large, “puppy dog” eyes, as if begging or pleading. May also represent adoration or feeling touched by a loving gesture."
-  }
+    },
+    {
+        id: 13,
+        emoji: "🥺",
+        name: "Pleading Face",
+        meaning: "A yellow face with furrowed eyebrows, a small frown, and large, “puppy dog” eyes, as if begging or pleading. May also represent adoration or feeling touched by a loving gesture."
+    },
+    {
+        id: 14,
+        emoji: "😋",
+        name: "Face Savouring Delicious Food",
+        meaning: "Bon appetit! Smiley is licking the corner of its mouth with its tongue. Is hungry, just cooked something very delicious or eaten a whole dish. Often interpreted as a grimace, with tongue outstretched, and used after a funny message or when fooling around."
+    },
+    {
+        id: 15,
+        emoji: "😜",
+        name: "Winking Eye",
+        meaning: "Smiley boldly sticks out the tongue, winking with one eye. Has made a joke or wants to flirt with the chat partner. Is carefree and does not mean it seriously."
+    },
+    {
+        id: 16,
+        emoji: "🤨",
+        name: "Face With a Raised Eyebrow",
+        meaning: "With the questioning look on the face, resembles the actor “The Rock“ (Dwayne Johnson). Can be used to express skepticism, disbelief or disapproval."
+    },
+    {
+        id: 17,
+        emoji: "🤓",
+        name: "Nerd Face",
+        meaning: "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+    },
+    {
+        id: 18,
+        emoji: "🥳",
+        name: "Face with party blower and party hat",
+        meaning: "That was a wild party or “Let's celebrate“. No matter what joyful event is in view, it will be celebrated appropriately. May also stand for the carnival season."
+    },
+    {
+        id: 19,
+        emoji: "🥵",
+        name: "Overheated face",
+        meaning: "It is so hot, I will get a heat stroke right away! Bright red head, the tongue is hanging from the mouth and drops of sweat are on the forehead. The hottest place in the world lies in Iran: In 2005, the highest temperature of 70.7 °C was measured in the Dasht-e Lut desert."
+    },
+    {
+        id: 20,
+        emoji: "🥶",
+        name: "Freezing face",
+        meaning: "It is so cold, you are about to freeze to death. The smile is almost frozen. Already totally blue in the face from the cold temperature, tortured facial expression. Only a cup of tea, coffee, or the hot tub can help now."
+    },
+    {
+        id: 21,
+        emoji: "🥶",
+        name: "Freezing face",
+        meaning: "It is so cold, you are about to freeze to death. The smile is almost frozen. Already totally blue in the face from the cold temperature, tortured facial expression. Only a cup of tea, coffee, or the hot tub can help now."
+    },
+    {
+        id: 22,
+        emoji: "🤥",
+        name: "Lying Face",
+        meaning: "As with Pinocchio, the nose becomes longer with each lie, which exposes him as a liar. Symbol of a falsehood, boast, or an unlikely story."
+    },
+    {
+        id: 23,
+        emoji: "🤤",
+        name: "Drooling Face",
+        meaning: "Slobbering face, the saliva flowing from the corner of the mouth. Represents desire for a person, delicious food, the new car, or the anticipation of an event. Expression of extreme interest and desire."
+    },
+    {
+        id: 24,
+        emoji: "👺",
+        name: "Japanese Goblin",
+        meaning: "The Tengu (literally: heavenly dog) is an evil and magical being from Japan. Has supernatural powers and brings forth disaster. The red mask has eyebrows and a beard and a noticeably long nose."
+    },
+    {
+        id: 25,
+        emoji: "👻",
+        name: "Ghost",
+        meaning: "The funny-smiling ghost is too cute to frighten others or cause harm. Symbol for Halloween or jokingly for creepy things, e.g. movies."
+    }
 ];
 
 export default emojipedia;
