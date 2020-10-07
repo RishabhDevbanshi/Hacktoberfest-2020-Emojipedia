5f46b786a1f49e5ba38dcb49254be15b3ce99256
parhan <apriliandi642@gmail.com>
2020-10-07 21:09:54 +0700

    add copy emoji button and intall module copy-to-clipboard

--- src/components/Emoji.jsx
+++ src/components/Emoji.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import copy from "copy-to-clipboard";
 
 export default function Emoji({ name, emoji, meaning }) {
+  function copyEmoji(emoji) {
+    copy(emoji);
+    alert("emojis copied....");
+  }
+
   return (
     <div className="term">
       <dt className="emoji-term">
@@ -10,6 +16,9 @@ export default function Emoji({ name, emoji, meaning }) {
       </dt>
 
       <dd>{meaning}</dd>
+      <br />
+
+      <button onClick={() => copyEmoji(emoji)} className="copy-emoji-btn">Copy Emoji</button>
     </div>
   );
 }
