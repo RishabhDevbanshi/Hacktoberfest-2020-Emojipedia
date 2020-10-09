5f71e045b49905b6fa5163e33aeaa603051cca71
Elizabeth Yu <elizabeth.yu@auspost.com.au>
2020-10-09 15:54:32 +1100

    Add tooltip hover

--- src/components/Emoji.jsx
+++ src/components/Emoji.jsx
@@ -4,9 +4,12 @@ function Emoji(props) {
   return (
     <div className="term">
       <dt>
-        <span className="emoji" role="img" aria-label={props.name}>
-          {props.emoji}
-        </span>
+        <div className="tooltip">
+          <span className="tooltiptext">{props.name}</span>
+          <span className="emoji" role="img" aria-label={props.name}>
+            {props.emoji}
+          </span>
+        </div>
       </dt>
       <dd>{props.meaning}</dd>
     </div>
